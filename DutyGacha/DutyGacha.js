3662e2c5ef147d86e1eb480f37cc076d317fe11b
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2020-01-05 17:38:43 +0900

    当番ガチャのコメントや文言修正

--- DutyGacha/DutyGacha.js
+++ DutyGacha/DutyGacha.js
@@ -1,36 +1,45 @@
+// POSTリクエスト時に当番ガチャを実行する
+// （トークン認証を行い、特定のSlackチャンネルからのリクエストのみ受付）
 function doPost(e) {
   const verifyToken = PropertiesService.getScriptProperties().getProperty('SLACK_OUTGOING_TOKEN');
-  
+
   if (verifyToken !== e.parameter.token) {
     throw new Error("トークンが違います。");
   }
-  dutyGacha();    
+  dutyGacha();
 }
 
+// スプレッドシートのデータを元に当番ガチャを行い、結果をSlackに投稿する
 function dutyGacha() {
+  // メンバー表のデータ部分のセル範囲
   const memberDataCellRange = 'A4:B23';
+  // 当番履歴表の開始列
   const historyDataColumnRangeStart = 'D';
+  // 当番履歴表の終了列
   const historyDataColumnRangeEnd = 'F';
+  // 当番履歴表の列範囲
   const historyDataColumnRange = historyDataColumnRangeStart + ':' + historyDataColumnRangeEnd;
+  // ガチャから除外する履歴件数
   const historyDataTargetNum = 5;
+  // 当番抽選の人数
   const dutyMemberNum = 2;
   try {
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('当番履歴票');
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('当番履歴表');
     const joinMemberList = getJoinMemberList(sheet, memberDataCellRange, dutyMemberNum);
     const historyMenberList = getHistoryMemberList(sheet, historyDataColumnRange, historyDataTargetNum, dutyMemberNum);
     const gachaMemberList = getGachaMemberList(joinMemberList, historyMenberList, dutyMemberNum);
     const dutyMemberList = getDutyMemberList(gachaMemberList, dutyMemberNum);
     var msg = createNoticeMsg(gachaMemberList, dutyMemberList);
-    
+
     const insertRow = sheet.getRange(historyDataColumnRangeStart + sheet.getMaxRows()).getNextDataCell(SpreadsheetApp.Direction.UP).getRow() + 1;
     const insertRange = historyDataColumnRangeStart + insertRow + ':' + historyDataColumnRangeEnd + insertRow;
     const insertDataArr = createInsertData(dutyMemberList);
     sheet.getRange(insertRange).setValues(insertDataArr);
   } catch (e) {
-    var msg = 'エラーが発生しました：' + e.message + '\nfileName：' + e.fileName + '\nlineNumber：' + e.lineNumber + '\nstack：\n' + e.stack; 
+    var msg = 'エラーが発生しました：' + e.message + '\nfileName：' + e.fileName + '\nlineNumber：' + e.lineNumber + '\nstack：\n' + e.stack;
     Logger.log(msg);
   }
-    
+
   try {
     const webHookUrl = PropertiesService.getScriptProperties().getProperty('WEBHOOK_URL');
     const jsonData =
@@ -50,7 +59,8 @@ function dutyGacha() {
   }
 }
 
-function getJoinMemberList(sheet,memberDataCellRange, dutyMemberNum) {
+// メンバー表からガチャ参加メンバーを抽出して配列で返す
+function getJoinMemberList(sheet, memberDataCellRange, dutyMemberNum) {
   const memberData = sheet.getRange(memberDataCellRange).getValues();
   var joinMemberList = [];
   memberData.map(function(data){
@@ -59,11 +69,12 @@ function getJoinMemberList(sheet,memberDataCellRange, dutyMemberNum) {
     }
   });
   if (joinMemberList.length < dutyMemberNum) {
-    throw new Error('ガチャ参加メンバーが抽選メンバー数より少ないです。ガチャ参加メンバーを増やしてください。');
+    throw new Error('ガチャ参加メンバーが当番抽選メンバー数より少ないです。ガチャ参加メンバーを増やしてください。');
   }
   return joinMemberList;
 }
 
+// 当番履歴表から指定件数分の履歴メンバーを抽出して配列で返す
 function getHistoryMemberList(sheet, historyDataColumnRange, historyDataTargetNum, dutyMemberNum) {
   const range = sheet.getRange(historyDataColumnRange);
   const historyData = range.getValues();
@@ -78,22 +89,23 @@ function getHistoryMemberList(sheet, historyDataColumnRange, historyDataTargetNu
       if (filterHistoryData[lastIndex - i][l].match(/当番.*/)) {
         return historyMenberList;
       }
-      historyMenberList.push(filterHistoryData[lastIndex - i][l]); 
+      historyMenberList.push(filterHistoryData[lastIndex - i][l]);
     }
   }
   return historyMenberList;
 }
 
+// ガチャ参加メンバーから履歴メンバーを除外したものを配列で返す
 function getGachaMemberList(joinMemberList, historyMenberList, dutyMemberNum) {
   const gachaMemberList = joinMemberList.filter(function(data){ return historyMenberList.indexOf(data) === -1});
   if (gachaMemberList.length < dutyMemberNum) {
-    throw new Error('ガチャ参加メンバーから履歴メンバーを除外した数が、抽選メンバー数より少ないです。ガチャ参加メンバーを増やしてください。'); 
+    throw new Error('ガチャ参加メンバーから履歴メンバーを除外した数が、抽選メンバー数より少ないです。ガチャ参加メンバーを増やしてください。');
   }
   return gachaMemberList;
 }
 
-//当番の抽選結果を返す
-function getDutyMemberList(gachaMenberList, dutyMemberNum) {  
+// 当番抽選メンバー数分、当番抽選を行い、当月の当番メンバーの配列を返す
+function getDutyMemberList(gachaMenberList, dutyMemberNum) {
   var dutyMemberList = [];
   for (var i = 0; i < dutyMemberNum; i++) {
     do {
@@ -101,16 +113,17 @@ function getDutyMemberList(gachaMenberList, dutyMemberNum) {
     } while (dutyMemberList.indexOf(gachaMenberList[index]) !== -1);
     dutyMemberList[i] = gachaMenberList[index];
   }
-  
+
   return dutyMemberList;
 }
 
-//0～(length-1)の乱数を返す
+// 0～(length-1)の乱数を返す
 function random(length) {
   //例　5人の場合 0~0.9999… * 5 の小数点切り捨てで、0~4になる
   return Math.floor(Math.random() * length);
 }
 
+// Slack通知メッセージを作成して返す
 function createNoticeMsg(gachaMenber, dutyMemberList) {
   var noticeMsg = '';
   dutyMemberList.forEach(function(member) {
@@ -119,6 +132,7 @@ function createNoticeMsg(gachaMenber, dutyMemberList) {
   return noticeMsg;
 }
 
+// スプレッドシートに記録する当月データの配列を返す
 function createInsertData(dutyMemberList) {
   var insertData = [Utilities.formatDate(new Date(), "JST", "yyyy/MM")];
   insertData = insertData.concat(dutyMemberList);
--- DutyGacha/README.md
+++ DutyGacha/README.md
@@ -13,19 +13,19 @@ OutgoingWebhookと連携させることで、Slackからトリガーワードで
 dutyGacha関数の最初に定義している以下の変数の値を、シートや状況に合わせて指定しておく。
 
 - memberDataCellRange
-　メンバー表のデータ部分（デフォルトはA4:B23）
+　メンバー表のデータ部分のセル範囲（デフォルトはA4:B23）
 
 - historyDataColumnRangeStart
-　履歴表の開始列（デフォルトはD）
+　当番履歴表の開始列（デフォルトはD）
 
 - historyDataColumnRangeEnd
-　履歴表の終了列（デフォルトはF）
+　当番履歴表の終了列（デフォルトはF）
 
 - historyDataTargetNum
-　何件分の履歴をガチャから除外するか（デフォルトは5）
+　過去何件分の履歴をガチャから除外するか（デフォルトは5）
 
 - dutyMemberNum
-　当番の人数（デフォルトは2）
+　当番抽選の人数（デフォルトは2）
 
 ### 通知内容
 <img src="./result.png">
